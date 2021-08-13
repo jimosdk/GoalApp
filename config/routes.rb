967cd4ad2dc1ac82e65fe9ab1130c94e42894cbe
jimosk <jimosdk@gmail.com>
2021-08-13 12:30:43 +0300

    Added top level resource users with actions new,create,show and top level resource session with actions new,create,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :users,only: [:show,:new,:create]
+  resource :session,only: [:new,:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
