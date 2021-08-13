ca5ff79dda87af33193acc094970b78a67298414
jimosk <jimosdk@gmail.com>
2021-08-13 17:41:35 +0300

    Created goal model ,implemented validations and belongs_to :user association along with corresponding tests

---
+++ app/models/goal.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: goals
+#
+#  id          :bigint           not null, primary key
+#  completed   :boolean          default(FALSE), not null
+#  description :text
+#  private     :boolean          default(FALSE), not null
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_goals_on_user_id  (user_id)
+#
+class Goal < ApplicationRecord
+    validates :title,presence: true
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+end
---
+++ spec/models/goal_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: goals
+#
+#  id          :bigint           not null, primary key
+#  completed   :boolean          not null
+#  description :text
+#  private     :boolean          not null
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_goals_on_user_id  (user_id)
+#
+require 'rails_helper'
+
+RSpec.describe Goal, type: :model do
+  it {should validate_presence_of(:title)}
+  
+  it {should belong_to(:user)}
+end
