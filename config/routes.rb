52ec3fc7d05e332de4e43e0023283076ae302779
jimosk <jimosdk@gmail.com>
2021-08-14 12:10:32 +0300

    Added action index to top level resource users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users,only: [:show,:new,:create]
+  resources :users,only: [:index,:show,:new,:create]
 
   resource :session,only: [:new,:create,:destroy]
 
