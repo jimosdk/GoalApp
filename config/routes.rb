c2de52423c4aa6f55adaa17763f63e85edc0d79b
jimosk <jimosdk@gmail.com>
2021-08-14 11:41:55 +0300

    Added top level resource goals with actions new,create,edit,update and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :users,only: [:show,:new,:create]
+
   resource :session,only: [:new,:create,:destroy]
+
+  resources :goals ,only: [:new,:create,:edit,:update,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
