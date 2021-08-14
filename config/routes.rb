10e5cd04020d287947ace5abbc1ec40691db644c
jimosk <jimosdk@gmail.com>
2021-08-14 19:57:11 +0300

    Added action :destroy to top level resource :goals

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resource :session,only: [:new,:create,:destroy]
 
-  resources :goals ,only: [:new,:create,:edit,:update,:show] do 
+  resources :goals ,only: [:new,:create,:edit,:update,:show,:destroy] do 
     patch :complete ,on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
