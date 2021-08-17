2d893ee2411478019abf05b9fc84b12e7dba39b5
jimosk <jimosdk@gmail.com>
2021-08-17 11:50:11 +0300

    Added top level resource goal_comments and user_comments with actions create,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  
   resources :users,only: [:index,:show,:new,:create]
 
   resource :session,only: [:new,:create,:destroy]
@@ -7,5 +8,8 @@ Rails.application.routes.draw do
   resources :goals ,only: [:new,:create,:edit,:update,:show,:destroy] do 
     patch :complete ,on: :member
   end
+
+  resources :goal_comments,only: [:create,:destroy]
+  resources :user_comments,only: [:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
